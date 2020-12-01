44df48215c91f526493c91b3df10ff02624aa867
ekaufmann <51715734+ekaufmann@users.noreply.github.com>
2020-12-01 12:09:44 -0300

    Swagger2 implemented - Aluno operations done

---
+++ src/main/java/com/example/demo/configuration/SwaggerConfiguration.java
@@ -0,0 +1,39 @@
+package com.example.demo.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket schoolAPI() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.demo"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+    private ApiInfo metaInfo() {
+        return new ApiInfo(
+                "School API REST",
+                "School managment API REST",
+                "1.0",
+                "Terms of Service",
+                new Contact("Eduardo", "url", "email"),
+                "License",
+                "License URL",
+                new ArrayList<>()
+        );
+    }
+}
