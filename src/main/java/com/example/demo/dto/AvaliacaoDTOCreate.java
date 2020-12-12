569ef76c25c1a56e79e561080dd4ea44db4163a0
ekaufmann <51715734+ekaufmann@users.noreply.github.com>
2020-12-11 21:11:29 -0300

    Avaliação size validation

--- src/main/java/com/example/demo/dto/AvaliacaoDTOCreate.java
+++ src/main/java/com/example/demo/dto/AvaliacaoDTOCreate.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Data
@@ -16,6 +17,7 @@ public class AvaliacaoDTOCreate {
     private Long disciplinaId;
 
     @NotNull(message = "Conteúdo precisa ser informado!")
+    @Size(min = 5, max = 64, message = "Conteúdo must be between 5 and 64")
     private String conteudo;
 
     @NotNull(message = "Data de realização precisa ser informada!")
--- src/main/java/com/example/demo/repository/MentoriaRepository.java
+++ src/main/java/com/example/demo/repository/MentoriaRepository.java
@@ -8,8 +8,6 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface MentoriaRepository extends JpaRepository<Mentoria, Long> {
 
